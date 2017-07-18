6fd3ca7aeaad462b491ce492bb4529f85b757ebe
Andrey Menshikh <andrey@light-tecnologies.com>
2017-07-19 02:59:49 +0300

    Added CORS hack for https

--- client/src/scripts/helpers/request.js
+++ client/src/scripts/helpers/request.js
@@ -2,8 +2,18 @@ import qs from 'querystring';
 
 const APPID = () => '454d9538142191fe4d6fec760075d53c';
 
+const CORS = () => {
+  let corsURL = '';
+
+  if (window.location.protocol === 'https:') {
+    corsURL = 'https://cors-anywhere.herokuapp.com/';
+  }
+
+  return corsURL;
+};
+
 const weatherAPIUrl = () =>
-  `http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=${APPID()}`;
+  `${CORS()}http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=${APPID()}`;
 
 const get = (params, successFunction, errorFunction) => {
   const paramsStringify = qs.stringify(params);
